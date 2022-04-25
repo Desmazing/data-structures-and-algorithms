1459263a253a8da3ee949dbe11de624304cac48b
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-04-25 15:56:14 +0300

    always learning

---
+++ Task25.py
@@ -0,0 +1,11 @@
+# get student marks, check for pass mark of 40
+# return pass if over 40, fail if less than 40
+
+name = (input('Enter name: ').lower())
+#marks = float(input('Enter marks: '))
+
+#if 0 < marks < 40: print(f'Sorry {name}, you have failed!')
+#else: print(f'Congratulations {name}, you have passed!')
+
+if name == 'romanus': print('No entry!')
+else: print('You can take the exam!')
