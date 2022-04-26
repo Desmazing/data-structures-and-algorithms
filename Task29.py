e114dcce32d3a1eda4098e6575e491f04bba4f51
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-04-26 11:26:11 +0300

    always learning

---
+++ Task29.py
@@ -0,0 +1,7 @@
+# user input
+# check whether positive, negative or equal to zero
+
+num = int(input('Enter a number: '))
+if num > 0: print(f'{num} is positive')
+elif num < 0: print(f'{num} is negative')
+elif num == 0: print('The number is Zero')
---
+++ Task30.py
@@ -0,0 +1,13 @@
+# get user number input
+# check divisibility by 5 or 6
+
+num = int(input('Enter a number: '))
+
+if num % 2 == 0 and num % 3 == 0 and num % 5 == 0:
+	print(f'{num} is divisible by 5 and 6')
+elif num % 2 == 0 and num % 3 == 0 and num % 5 != 0:
+	print(f'{num} is divisible by 6 but not 5')
+elif num % 6 != 0 and num % 5 == 0:
+	print(f'{num} is divisible by 5 but not 6')
+elif num % 6 != 0 and num % 5 == 0:
+	print(f'{num} is divisible by neither 5 nor 6')
