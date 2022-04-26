b5f272cf105edd2db6c33fbe3296f847bb3e544d
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-04-26 19:04:41 +0300

    always learning

---
+++ Task38.py
@@ -0,0 +1,13 @@
+# in math module, mat.degrees(angle) conv angle to degrees
+# math.radians(angle) conv angle to radians
+
+import math
+
+rad = int(input('Enter angle in radians: '))
+deg = math.degrees(rad)
+print(deg)
+
+
+deg = int(input('Enter angle in degrees: '))
+rad = math.radians(deg)
+print(rad)
