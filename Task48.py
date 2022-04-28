78271ca542982f5139bda3fd342e00d647ab966c
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-04-28 11:32:31 +0300

    simple python algorithms

---
+++ Task48.py
@@ -0,0 +1,15 @@
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+
+numl = [a, b]
+
+print('Values before swapping are: ')
+print(f'\ta = {a}')
+print(f'\tb = {b}')
+
+a = numl[1]
+b = numl[0]
+
+print('Values after swapping are: ')
+print(f'\ta = {a}')
+print(f'\tb = {b}')
---
+++ Task49.py
@@ -0,0 +1,7 @@
+# convert seconds to hours
+
+sec = float(input('Enter time in seconds: '))
+
+hr = sec /3600
+
+print(f'{sec} second(s) = {hr} hour(s)')
