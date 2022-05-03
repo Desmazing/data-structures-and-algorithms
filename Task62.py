5575eaae1f7cc62b75e3cccb10dd9e8d3ecbdb78
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-05-03 15:37:49 +0300

    Simple Python algorithms

---
+++ Task62.py
@@ -0,0 +1,26 @@
+# get info from users
+# display info in order
+# update cnic, age and contact
+# display updated results
+
+name = input("Enter student name: ")
+father_name = input("Enter father's name: ")
+cnic = input("Enter CNIC: ")
+age = input("Enter age: ")
+contact = input("Enter contact info: ")
+
+print(f"Name \t\t {name}")
+print(f"Father Name \t {father_name}")
+print(f"CNIC \t\t {cnic}")
+print(f"Age \t\t {age}")
+print(f"Contact \t {contact}")
+
+cnic = input("\nEnter CNIC: ")
+age = input("Enter age: ")
+contact = input("Enter contact info: ")
+
+print(f"\nName \t\t {name}")
+print(f"Father Name \t {father_name}")
+print(f"CNIC \t\t {cnic}")
+print(f"Age \t\t {age}")
+print(f"Contact \t {contact}")
