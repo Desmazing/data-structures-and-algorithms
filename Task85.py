8bd89f71d1861347411aff7d11eb417dab8eb23e
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-05-10 10:04:08 +0300

    Python Algorithms

---
+++ Task85.py
@@ -0,0 +1,13 @@
+# get name that ends in 'n' from list
+
+names = ["Allan", "Dennis","Alain","Dan"]
+
+for name in names:
+	if name.endswith('n'):
+		print(name)
+
+print('\n')
+
+for name in names:
+	if name.startswith("A"):
+		print(name)
