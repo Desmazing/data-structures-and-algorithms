5da9d260a4baafc409ce0486465d9462ada8a4c6
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-05-10 10:05:30 +0300

    Python Algorithms

---
+++ Task88.py
@@ -0,0 +1,10 @@
+# get number from user
+# find half the number
+# subtract the half
+
+num = int(input("Enter number: "))
+
+half_num = num / 2
+
+print(f'Half number is {half_num}')
+print(f'Result is {num - half_num}')
