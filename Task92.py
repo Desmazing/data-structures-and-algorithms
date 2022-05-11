d398feda942b628bc088ad0d69e490f59afadb87
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-05-11 10:43:29 +0300

    Python Algorithms

---
+++ Task92.py
@@ -0,0 +1,8 @@
+# find ceiling and floor of a number
+# nearest integer up and down respectively
+
+import math
+
+num = float(input("Enter a number: "))
+print(f'Ceiling of {num} is {math.ceil(num)}')
+print(f'Floor of {num} is {math.floor(num)}')
