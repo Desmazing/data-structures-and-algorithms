b885c4635a07368a68a4fdf2faeb8f3497924d57
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-05-13 09:48:56 +0300

    Python Algorithms

---
+++ Task98.py
@@ -0,0 +1,11 @@
+# square root of list middle element
+
+import math
+
+lst = [1,24,24,54,12,65,12,65]
+
+print(f'List length is {len(lst)}')
+
+a = int(len(lst) / 2)
+
+print(f'Square root of mid element in list, {lst[a]} is {math.sqrt(lst[a])}')
