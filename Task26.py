0ebc588935ade3fb5e4b96728fc7b8da524c37f9
Desmond O <100370067+Desmazing@users.noreply.github.com>
2022-10-07 22:41:39 +0300

    even_odd number

--- Task26.py
+++ Task26.py
@@ -6,3 +6,6 @@ num1 =  int(input('Enter a number: '))
 if num1 % 2 == 0: print('Even number.');
 else: print('Odd number.')
 print(f'Square of {num1} is {num1 * num1}')
+
+def odd_even(num1):
+  return "Odd number." if num1%2 else "Even number."
